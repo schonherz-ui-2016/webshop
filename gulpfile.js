24b7ee415cee3dc09cb516b29aee230ff8f3e515
Misi <szatmari.mihaly96@gmail.com>
2016-12-06 17:57:23 +0100

    Modyfied gulpfile.js #1

--- gulpfile.js
+++ gulpfile.js
@@ -4,13 +4,13 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 
 gulp.task('sass', function() {
-     return gulp.src('./app/**/*.scss')
+     return gulp.src('./scss/**/*.scss')
         .pipe(sass().on('error', sass.logError))
         .pipe(gulp.dest('./app/css/'))
 });
 
 gulp.task('sass:watch', function () {
-    gulp.watch('./app/**/*.scss', ['sass']);
+    gulp.watch('./scss/**/*.scss', ['sass']);
 });
 
 gulp.task('default', ['sass:watch']);
