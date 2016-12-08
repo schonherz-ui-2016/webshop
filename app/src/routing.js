ed2a88391a9ff9af48e63412faa5cf4aa1b81211
Marton Krisztian Zsolt <krisztianm@mailbox.unideb.hu>
2016-12-08 20:39:34 +0100

    Removed route in routing.js #21

--- app/src/routing.js
+++ app/src/routing.js
@@ -7,9 +7,6 @@
                     templateUrl: 'main.html',
                     controller: 'ProductListCtrl'
                 })
-                .when('/product/1', {
-                    templateUrl: 'empty.html'
-                })
                 .when('/product/:id', {
                     templateUrl: 'productDetails.html'
                 });
