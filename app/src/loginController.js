96cdaca9f3f0f96e7106a4244ca104bca1f41b7f
szimeonovn <szimi7@gmail.com>
2016-12-09 10:46:14 +0100

    Need advice on issue #19

---
+++ app/src/loginController.js
@@ -0,0 +1,21 @@
+(function () {
+    var token = "";
+    angular
+        .module('webshopModule')
+        .controller('LoginCtrl', LoginController);
+    function LoginController($http, $scope, $location) {
+        $scope.login = function (email, password) {
+            $http.post('http://localhost:1337/user/login', {
+                "email": email,
+                "password": password
+            }).then(function (result) {
+                token = result.data.token;
+                //$location.path("/login");
+            }, function () {
+                //errorCallback
+            });
+
+        }
+        login($scope.email, $scope.password);
+    }
+})();
