4f475f048ed1dd35e12dc08f842e8c5f73cabf42
szimeonovn <szimi7@gmail.com>
2016-12-15 13:46:35 +0100

    Created api.js #36

---
+++ app/src/api.js
@@ -0,0 +1,10 @@
+(function () {
+    var urlBase = 'http://localhost:1337';
+    angular
+        .module('webshopModule')
+        .service('api', Service)
+
+    function Service($http) {
+        
+    }
+})();
