01da717c68bd8cafb33720d188b6553982f12096
szimeonovn <szimi7@gmail.com>
2017-01-02 18:51:52 +0100

    Removed comments #44

--- app/src/headerController.js
+++ app/src/headerController.js
@@ -7,14 +7,3 @@
         });
 
 })();
-//console.log(login.getToken());
-
-//login.getToken() == UNDEFINED is true --> error ág not valid email+jelszó -->login, sign up stays
-//login.getToken() == NOT UNDEF is false--> valid -> logged in--> login/signup removal --->welcome message
-
-/*
- console.log("sikertelen "+ $scope.unsuccessfulLogin);
- console.log("sikeres"+ $scope.successfulLogin);
-
- */
-
