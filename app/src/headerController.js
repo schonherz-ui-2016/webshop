9546ed4b4c479b260f156c6158dcad6d24857443
szimeonovn <szimi7@gmail.com>
2017-01-15 20:53:47 +0100

    Welcome the user by username #74

--- app/src/headerController.js
+++ app/src/headerController.js
@@ -2,7 +2,7 @@
     angular
         .module('webshopModule')
 
-        .controller('headerCtrl', function ($scope, loginService) {
+        .controller('headerCtrl', function ($scope, loginService, api) {
             function fetchSession() {
                 $scope.session = loginService.getSession();
             }
@@ -13,6 +13,21 @@
             $scope.logout = function () {
                 loginService.logout();
             };
+
+            var session = loginService.getSession();
+
+            function getUser() {
+                api.getUserId(session.token)
+                    .then(function (result) {
+                        api.getUser(session.token, result.data.id)
+                            .then(function (result) {
+                                $scope.user = result.data;
+                            })
+                    })
+            }
+
+            getUser();
+
         });
 
 })();
