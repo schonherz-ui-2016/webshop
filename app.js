460635e69427effe7a360eaa53a090ff442b2861
Marton Krisztian Zsolt <krisztianm@mailbox.unideb.hu>
2017-01-16 15:47:29 +0100

    Port configuration #94

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ app.get('/', function (req, res) {
     res.sendFile('app/index.html', {root: __dirname});
 });
 
-app.listen(3000, function () {
-    console.log('App listening on port 3000!')
+var port = process.env.PORT || 3000;
+
+app.listen(port, function () {
+    console.log('App listening on port ' + port + '!')
 });
