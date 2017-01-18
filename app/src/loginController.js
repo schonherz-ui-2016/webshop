ece8402442d0975f8625ac5f9c75754102d191c5
szimeonovn <szimi7@gmail.com>
2017-01-18 21:17:45 +0100

    Remember me checkbox fixed #75

--- app/src/loginController.js
+++ app/src/loginController.js
@@ -2,12 +2,12 @@
 
     angular.module('webshopModule')
         .controller('loginCtrl', function ($scope, $location, loginService) {
-
             $scope.login = function () {
-                loginService.login($scope.email, $scope.password)
+                loginService.login($scope.email, $scope.password, $scope.rememberMe)
                     .then(function () {
                         $location.path("/");
                     });
             };
+
         })
 })();
--- app/src/loginService.js
+++ app/src/loginService.js
@@ -4,20 +4,26 @@
         .service('loginService', LoginService);
 
     function LoginService(api, $window, $rootScope) {
+
         var token = $window.localStorage.getItem('token');
+
         var session = {
             authenticated: !!token,
             token: token
         };
 
-        this.login = function (email, password) {
-            return api.login(email, password)
+        this.login = function (email, password, rememberMe) {
+            return api.login(email, password, rememberMe)
                 .then(function (token) {
                     session.authenticated = true;
                     session.token = token;
-                    $window.localStorage.setItem('token', token);
+                    $window.sessionStorage.setItem('token', token);
                     $rootScope.$broadcast('loginStateChange', true);
+                    if (rememberMe) {
+                        $window.localStorage.setItem('token', token);
+                    }
                 });
+
         };
 
         this.getSession = function () {
@@ -26,6 +32,7 @@
 
         this.logout = function logout() {
             $window.localStorage.removeItem('token');
+            $window.sessionStorage.removeItem('token');
             session.token = undefined;
             session.authenticated = false;
             $rootScope.$broadcast('loginStateChange', false);
